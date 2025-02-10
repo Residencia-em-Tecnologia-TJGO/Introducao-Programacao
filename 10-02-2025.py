07dad240ebea94a6eb938627df00e19dd475bc3a
samuel.victor <samuel.victor@ebserh.gov.br>
2025-02-10 17:26:35 -0300

    conjectura de collatz

--- 10-02-2025.py
+++ 10-02-2025.py
@@ -1,8 +1,21 @@
 # AULA 10/02/2025
+# python3 10-02-2025.py
+
 # Questão 1.1)
+# def bird__pad(str):
+#     sound = "bird"
+#     return sound + str + sound
 
-def bird__pad(str):
-    sound = "bird"
-    return sound + str + sound
+# print(bird__pad("som"))
 
-print(bird__pad("som"))
+
+# A conjectura de Collatz
+def collatz(x):
+    while x > 1:
+        if x % 2 == 0:
+            x = x // 2
+        else:
+            x = 3 * x + 1
+    print(x)
+
+collatz(2)
